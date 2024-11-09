f9b4d87a7350733bdcf417102330917fb120bf75
richtervanemmerik <richtervanemmerik@gmail.com>
2024-11-09 17:00:18 +0100

    run the vec2text model script + beirbenchmark script

---
+++ beir_benchmark.py
@@ -0,0 +1,25 @@
+from beir import util, LoggingHandler
+from beir.retrieval import models
+from beir.datasets.data_loader import GenericDataLoader
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
+
+import logging
+import pathlib, os
+
+#### Just some code to print debug information to stdout
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+#### /print debug information to stdout
+
+#### Download scifact.zip dataset and unzip the dataset
+dataset = "quora"
+url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
+out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "datasets")
+data_path = util.download_and_unzip(url, out_dir)
+
+#### Provide the data_path where scifact has been downloaded and unzipped
+corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
+print(corpus)
---
+++ run_the_model.py
@@ -0,0 +1,33 @@
+import os
+import vec2text
+import torch
+from openai import OpenAI
+import math
+
+os.environ["OPENAI_API_KEY"] = ""
+
+def get_embeddings_openai(text_list, model="text-embedding-ada-002") -> torch.Tensor:
+    client = OpenAI()  # Initialize client
+    batches = math.ceil(len(text_list) / 128)
+    outputs = []
+    for batch in range(batches):
+        text_list_batch = text_list[batch * 128 : (batch + 1) * 128]
+        response = client.embeddings.create(  # Updated API call
+            input=text_list_batch,
+            model=model,
+        )
+        outputs.extend([e.embedding for e in response.data])  # Updated response structure
+    return torch.tensor(outputs)
+
+corrector = vec2text.load_pretrained_corrector("text-embedding-ada-002")
+embeddings = get_embeddings_openai([
+       "Jack Morris is a PhD student at Cornell Tech in New York City",
+       "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity"
+])
+print(embeddings.shape)
+text = vec2text.invert_embeddings(
+    embeddings=embeddings.cuda(),
+    corrector=corrector
+)
+
+print(text)
