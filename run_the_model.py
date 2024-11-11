30b6aa405724513d63908d5b035db53a3c8c0a0c
Ryan Ott <ott.r21@gmail.com>
2024-11-11 18:30:17 +0100

    Spiced up the run script

--- run_the_model.py
+++ run_the_model.py
@@ -1,13 +1,22 @@
-import os
-import vec2text
-import torch
-from openai import OpenAI
+import argparse
 import math
+import os
+import torch
+import vec2text
+from openai import OpenAI
+from utils import halo
 
-os.environ["OPENAI_API_KEY"] = ""
 
+def get_openai_api_key() -> str:
+    api_key = os.getenv("OPENAI_API_KEY")
+    if not api_key:
+        raise ValueError("OPENAI_API_KEY environment variable is not set")
+    return api_key
+
+
+@halo("Loading OpenAI embeddings")
 def get_embeddings_openai(text_list, model="text-embedding-ada-002") -> torch.Tensor:
-    client = OpenAI()  # Initialize client
+    client = OpenAI(api_key=get_openai_api_key())
     batches = math.ceil(len(text_list) / 128)
     outputs = []
     for batch in range(batches):
@@ -19,15 +28,31 @@ def get_embeddings_openai(text_list, model="text-embedding-ada-002") -> torch.Te
         outputs.extend([e.embedding for e in response.data])  # Updated response structure
     return torch.tensor(outputs)
 
-corrector = vec2text.load_pretrained_corrector("text-embedding-ada-002")
-embeddings = get_embeddings_openai([
-       "Jack Morris is a PhD student at Cornell Tech in New York City",
-       "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity"
-])
-print(embeddings.shape)
-text = vec2text.invert_embeddings(
-    embeddings=embeddings.cuda(),
-    corrector=corrector
-)
 
-print(text)
+@halo("Loading corrector model")
+def get_corrector(model_name: str) -> vec2text.models.CorrectorEncoderModel:
+    if model_name == "ada":
+        corrector = vec2text.load_pretrained_corrector("text-embedding-ada-002")
+    else:
+        raise ValueError(f"Unknown corrector model name: {model_name}")
+    return corrector
+
+
+def main(args):
+    corrector = get_corrector(args.model)
+    embeddings = get_embeddings_openai(args.text)
+    text = vec2text.invert_embeddings(
+        embeddings=embeddings.cuda(),
+        corrector=corrector,
+    )
+    print(text)
+    print(type(text))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="ada", help="Corrector model name")
+    parser.add_argument("--text", type=str, nargs="+", default=["Jack Morris is a PhD student at Cornell Tech in New York City", "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity"], help="List of strings to embed")
+    args = parser.parse_args()
+    
+    main(args)
